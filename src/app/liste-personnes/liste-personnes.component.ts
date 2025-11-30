1c9932e2d2cc188f1a3009f93104e447eaa25bd2
thetoramizu <beusset@hotmail.fr>
2025-11-30 17:23:55 +0100

    personnechoisie

--- src/app/liste-personnes/liste-personnes.component.ts
+++ src/app/liste-personnes/liste-personnes.component.ts
@@ -1,5 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { EnvelopeComponent } from "../envelope/envelope.component";
+import { PersonnesService } from '../services/personnes.service';
 
 @Component({
   selector: 'app-liste-personnes',
@@ -7,12 +8,15 @@ import { EnvelopeComponent } from "../envelope/envelope.component";
   templateUrl: './liste-personnes.component.html',
   styleUrl: './liste-personnes.component.scss',
 })
-export class ListePersonnesComponent {
+export class ListePersonnesComponent implements OnInit {
+
+  personneService = inject(PersonnesService)
+
   listPersonnes = signal<Personnes[]>([
-    { nom: 'Estelle', anniversaire: new Date(1990, 8, 11), svg: 'assets/gift_es.svg' },
+        { nom: 'Noélie', anniversaire: new Date(1990, 10, 30), svg: 'assets/gift_no.svg' },
     { nom: 'Sullyvan', anniversaire: new Date(1990, 8, 20), svg: 'assets/gift_su.svg' },
-    { nom: 'Noélie', anniversaire: new Date(1990, 10, 30), svg: 'assets/gift_no.svg' },
-  ]);
+  { nom: 'Estelle', anniversaire: new Date(1990, 8, 11), svg: 'assets/gift_es.svg' },
+]);
 
   anniToday = computed(() => {
     return this.listPersonnes().filter((personne) =>
@@ -28,6 +32,12 @@ export class ListePersonnesComponent {
 
   todayDate = signal<Date>(new Date());
 
+  ngOnInit() {
+    if(this.anniToday().length > 0) {
+      this.chooseWho(this.anniToday()[0]);
+    }
+  }
+
   isBirthday(dateNaissance: Date | string): boolean {
     const today = this.todayDate();
     const birth = new Date(dateNaissance);
@@ -40,6 +50,10 @@ export class ListePersonnesComponent {
       birth.getMonth() === today.getMonth()
     );
   }
+
+  chooseWho(personne: Personnes) {
+    this.personneService.personneChoisie.set(personne);
+}
 }
 
 export interface Personnes {
